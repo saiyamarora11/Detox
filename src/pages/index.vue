dc48cb21ee1e548b56d8d65217de16450cbe7da9
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2023-03-16 12:10:16 +0530

    intializeTheProject

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+//dependencies
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <div class="flex gap-x-2 mt-10">
+      <div class="font-bold text-4xl text-blue-500 mt-4">Snack It</div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
